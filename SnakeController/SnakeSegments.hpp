59bf04c0b56080f1f67fefba2dbc0c1f35e70348
Tomasz Wojtun <tomasz.wojtun16@gmail.com>
2022-07-15 18:24:39 +0200

    Now they are seriously added

---
+++ SnakeController/SnakeSegments.hpp
@@ -0,0 +1,59 @@
+#pragma once
+#include "SnakeInterface.hpp"
+#include "SnakeController.hpp"
+
+
+    namespace Snake{
+        namespace{
+bool isHorizontal(Direction direction)
+{
+    return Direction_LEFT == direction or Direction_RIGHT == direction;
+}
+
+bool isVertical(Direction direction)
+{
+    return Direction_UP == direction or Direction_DOWN == direction;
+}
+
+bool isPositive(Direction direction)
+{
+    return (isVertical(direction) and Direction_DOWN == direction)
+        or (isHorizontal(direction) and Direction_RIGHT == direction);
+}
+
+bool perpendicular(Direction dir1, Direction dir2)
+{
+    return isHorizontal(dir1) == isVertical(dir2);
+}
+        }
+
+class SnakeSegments{
+    public:
+    void setDirection(Snake::Direction d){
+        m_currentDirection = d;
+    }
+    struct Segment
+    {
+        int x;
+        int y;
+    };
+    Segment calculateNewHead() const{
+        Segment const& currentHead = m_segments.front();
+
+    Segment newHead;
+    newHead.x = currentHead.x + (isHorizontal(m_currentDirection) ? isPositive(m_currentDirection) ? 1 : -1 : 0);
+    newHead.y = currentHead.y + (isVertical(m_currentDirection) ? isPositive(m_currentDirection) ? 1 : -1 : 0);
+
+    return newHead;
+    }
+    void setSegment(int newX, int newY){
+        Segment seg{newX,newY};
+        m_segments.push_back(seg);
+    }
+    std::list<Segment> m_segments;
+    Direction m_currentDirection;
+
+
+};
+    }
+//namespace Snake
---
+++ SnakeController/SnakeWorld.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "SnakeController.hpp"
+#include "IPort.hpp"
+namespace Snake{
+class SnakeWorld{
+
+public:
+    SnakeWorld(IPort& displayPort, IPort& foodPort, IPort& scorePort): 
+    m_displayPort(displayPort), m_foodPort(foodPort), m_scorePort(scorePort) {}
+    
+    IPort& m_displayPort;
+    IPort& m_foodPort;
+    IPort& m_scorePort;
+
+    std::pair<int, int> m_mapDimension;
+    std::pair<int, int> m_foodPosition;
+
+};
+}
